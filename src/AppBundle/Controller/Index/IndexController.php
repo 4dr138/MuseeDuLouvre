32ed14ea0e1b556c318a8e1e7dce4f65ba6ee304
4dr138 <agautier38@gmail.com>
2017-12-29 16:06:18 +0100

    Modification du code selon bonnes pratiques

--- src/AppBundle/Controller/Index/IndexController.php
+++ src/AppBundle/Controller/Index/IndexController.php
@@ -16,46 +16,42 @@ use AppBundle\Form\Type\BilletType;
 class IndexController extends Controller
 {
   /**
-   * @Route("/", name="")
+   * @Route("/", name="homepage")
    */
   public function indexAction()
   {
     $newBasket = new Basket();
-    $formBuilderBasket = $this->get('form.factory')->createBuilder(BasketType::class, $newBasket);
-    $newBillet = new Billet();
-    $formBuilderBillet = $this->get('form.factory')->createBuilder(BilletType::class, $newBillet);
+    $formBasket = $this->createForm("AppBundle\Form\Type\BasketType", $newBasket);
 
-    $formBasket = $formBuilderBasket->getForm();
-    $formBillet = $formBuilderBillet->getForm();
-
-    return $this->render('index/index.html.twig', array('formBasket' => $formBasket->createView(), 'formBillet' => $formBillet->createView()));
-  }
-
-  /**
-   * @Route("/reservation", name="reservation")
-   * @Method({"GET","POST"})
-   */
-  public function newBasket(Request $request)
-  {
-// Génération du premier formulaire qui contient les informations de la commande
-     $newBasket = new Basket();
-
-     $formbuilderBasket = $this->get('form.factory')->createBuilder(FormType::class, $newBasket);
-
-    $formBasket = $formbuilderBasket->getForm();
-
-
-      if ($formBasket->isValid() && $formBasket->isSubmitted()) {
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($newBasket);
-        $em->flush();
-
-        $this->addFlash('infos', 'Informations générales enregistrées.');
-
-        return $this->redirectToRoute('reservation', array('id' => $newBasket->getId()));
-      }
-
-    return $this->render('reservation/reservation.html.twig', array('formBasket' => $formBasket->createView()));
+    return $this->render('index/index.html.twig', array('formBasket' => $formBasket->createView()));
   }
 }
+
+//   /**
+//    * @Route("/reservation", name="reservation")
+//    * @Method({"GET","POST"})
+//    */
+//   public function newBasket(Request $request)
+//   {
+// // Génération du premier formulaire qui contient les informations de la commande
+//      $newBasket = new Basket();
+//
+//      $formbuilderBasket = $this->get('form.factory')->createBuilder(FormType::class, $newBasket);
+//
+//     $formBasket = $formbuilderBasket->getForm();
+//
+//
+//       if ($formBasket->isValid() && $formBasket->isSubmitted()) {
+//
+//         $em = $this->getDoctrine()->getManager();
+//         $em->persist($newBasket);
+//         $em->flush();
+//
+//         $this->addFlash('infos', 'Informations générales enregistrées.');
+//
+//         return $this->redirectToRoute('reservation', array('id' => $newBasket->getId()));
+//       }
+//
+//     return $this->render('reservation/reservation.html.twig', array('formBasket' => $formBasket->createView()));
+//   }
+// }
